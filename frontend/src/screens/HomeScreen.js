737fff1c7794eedce95089709a21a23165794d38
Mubleem <102492190+Mublin@users.noreply.github.com>
2022-08-30 21:58:39 +0100

    go

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -46,7 +46,7 @@ function HomeScreen () {
         <h1>Featured Products</h1>
     <div className='products'> 
       {loading ? <LoadingBox /> :
-      error ? <MessageBox variant="danger">{error}</MessageBox> :(<Row> {products.map(product=> ( 
+      error ? <MessageBox variant="danger">{getError(error)}</MessageBox> :(<Row> {products.map(product=> ( 
       <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
       <Product product={product}></Product>  
     </Col>
